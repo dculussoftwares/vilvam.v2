8f87585757fb7ba34c1aef8022281ea4ab17619d
Natheeshkumar <natheeshkumar.rangasamy@rxglobal.com>
2024-03-15 20:36:11 +0530

    Remove bottom

--- lib/component/patient_options.dart
+++ lib/component/patient_options.dart
@@ -30,7 +30,6 @@ class PatientOptions extends StatelessWidget {
           HealeeOptions(),
           ClinicOptions(),
           DataExportOptions(),
-          BottomSheetSection(),
           Dialogs(),
           Dividers(),
           // PatientsOptions(),
@@ -1708,129 +1707,6 @@ class SnackBarSection extends StatelessWidget {
           'Show snackbar',
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
-      ),
-    );
-  }
-}
-
-class BottomSheetSection extends StatefulWidget {
-  const BottomSheetSection({super.key});
-
-  @override
-  State<BottomSheetSection> createState() => _BottomSheetSectionState();
-}
-
-class _BottomSheetSectionState extends State<BottomSheetSection> {
-  bool isNonModalBottomSheetOpen = false;
-  PersistentBottomSheetController? _nonModalBottomSheetController;
-
-  @override
-  Widget build(BuildContext context) {
-    List<Widget> buttonList = <Widget>[
-      IconButton(onPressed: () {}, icon: const Icon(Icons.share_outlined)),
-      IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
-      IconButton(onPressed: () {}, icon: const Icon(Icons.delete_outline)),
-      IconButton(onPressed: () {}, icon: const Icon(Icons.archive_outlined)),
-      IconButton(onPressed: () {}, icon: const Icon(Icons.settings_outlined)),
-      IconButton(onPressed: () {}, icon: const Icon(Icons.favorite_border)),
-    ];
-    List<Text> labelList = const <Text>[
-      Text('Share'),
-      Text('Add to'),
-      Text('Trash'),
-      Text('Archive'),
-      Text('Settings'),
-      Text('Favorite')
-    ];
-
-    buttonList = List.generate(
-        buttonList.length,
-        (index) => Padding(
-              padding: const EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 20.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  buttonList[index],
-                  labelList[index],
-                ],
-              ),
-            ));
-
-    return ComponentDecoration(
-      label: 'Bottom sheet',
-      tooltipMessage: 'Use showModalBottomSheet<T> or showBottomSheet<T>',
-      child: Wrap(
-        alignment: WrapAlignment.spaceEvenly,
-        children: [
-          TextButton(
-            child: const Text(
-              'Show modal bottom sheet',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            onPressed: () {
-              showModalBottomSheet<void>(
-                showDragHandle: true,
-                context: context,
-                // TODO: Remove when this is in the framework https://github.com/flutter/flutter/issues/118619
-                constraints: const BoxConstraints(maxWidth: 640),
-                builder: (context) {
-                  return SizedBox(
-                    height: 150,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 32.0),
-                      child: ListView(
-                        shrinkWrap: true,
-                        scrollDirection: Axis.horizontal,
-                        children: buttonList,
-                      ),
-                    ),
-                  );
-                },
-              );
-            },
-          ),
-          TextButton(
-            child: Text(
-              isNonModalBottomSheetOpen
-                  ? 'Hide bottom sheet'
-                  : 'Show bottom sheet',
-              style: const TextStyle(fontWeight: FontWeight.bold),
-            ),
-            onPressed: () {
-              if (isNonModalBottomSheetOpen) {
-                _nonModalBottomSheetController?.close();
-                setState(() {
-                  isNonModalBottomSheetOpen = false;
-                });
-                return;
-              } else {
-                setState(() {
-                  isNonModalBottomSheetOpen = true;
-                });
-              }
-
-              _nonModalBottomSheetController = showBottomSheet(
-                elevation: 8.0,
-                context: context,
-                // TODO: Remove when this is in the framework https://github.com/flutter/flutter/issues/118619
-                constraints: const BoxConstraints(maxWidth: 640),
-                builder: (context) {
-                  return SizedBox(
-                    height: 150,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 32.0),
-                      child: ListView(
-                        shrinkWrap: true,
-                        scrollDirection: Axis.horizontal,
-                        children: buttonList,
-                      ),
-                    ),
-                  );
-                },
-              );
-            },
-          ),
-        ],
       ),
     );
   }
